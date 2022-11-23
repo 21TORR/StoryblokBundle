3e69de9f3da6652c5259c87b17c3eb2ff478286e
Jannik Zschiesche <j.zschiesche@21torr.com>
2022-11-23 21:50:14 +0100

    Add transform + validate for markdown

--- src/Field/Definition/MarkdownField.php
+++ src/Field/Definition/MarkdownField.php
@@ -2,8 +2,11 @@
 
 namespace Torr\Storyblok\Field\Definition;
 
+use Symfony\Component\Validator\Constraints\Type;
+use Torr\Storyblok\Context\StoryblokContext;
 use Torr\Storyblok\Field\FieldType;
 use Torr\Storyblok\Validator\DataValidator;
+use Torr\Storyblok\Visitor\DataVisitorInterface;
 
 final class MarkdownField extends AbstractField
 {
@@ -49,6 +52,31 @@ final class MarkdownField extends AbstractField
 	 */
 	public function validateData (DataValidator $validator, array $contentPath, mixed $data) : void
 	{
-		// @todo add implementation
+		$validator->ensureDataIsValid(
+			$contentPath,
+			$this,
+			$data,
+			[
+				new Type("string"),
+			],
+		);
 	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function transformData (
+		mixed $data,
+		StoryblokContext $dataContext,
+		?DataVisitorInterface $dataVisitor = null,
+	) : mixed
+	{
+		// Normalize to null. Trim for checking, but don't trim data if is not empty, just to be sure.
+		$transformed = "" !== \trim($data)
+			? $data
+			: null;
+
+		return parent::transformData($transformed, $dataContext, $dataVisitor);
+	}
+
 }
