602dbffda057f06ddb0126b6f442cbe0e67bc6f8
Jannik Zschiesche <j.zschiesche@21torr.com>
2022-11-23 21:48:12 +0100

    Add validation + transformation for date time field

--- src/Field/Definition/DateTimeField.php
+++ src/Field/Definition/DateTimeField.php
@@ -2,11 +2,17 @@
 
 namespace Torr\Storyblok\Field\Definition;
 
+use Symfony\Component\Validator\Constraints\DateTime;
+use Symfony\Component\Validator\Constraints\Type;
+use Torr\Storyblok\Context\StoryblokContext;
 use Torr\Storyblok\Field\FieldType;
 use Torr\Storyblok\Validator\DataValidator;
+use Torr\Storyblok\Visitor\DataVisitorInterface;
 
 final class DateTimeField extends AbstractField
 {
+	private const DATE_TIME_FORMAT = "Y-m-d H:i";
+
 	/**
 	 * @inheritDoc
 	 */
@@ -45,6 +51,44 @@ final class DateTimeField extends AbstractField
 	 */
 	public function validateData (DataValidator $validator, array $contentPath, mixed $data) : void
 	{
-		// @todo add implementation
+		$validator->ensureDataIsValid(
+			$contentPath,
+			$this,
+			$data,
+			[
+				new Type("string"),
+				new DateTime(self::DATE_TIME_FORMAT),
+			],
+		);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function transformData (
+		mixed $data,
+		StoryblokContext $dataContext,
+		?DataVisitorInterface $dataVisitor = null,
+	) : mixed
+	{
+		$transformed = null !== $data
+			? \DateTimeImmutable::createFromFormat(self::DATE_TIME_FORMAT, $data)
+			: null;
+
+		if (false === $transformed)
+		{
+			$dataContext->logger->error("Encountered invalid date time: {value}", [
+				"value" => $data,
+			]);
+			$transformed = null;
+		}
+
+		// force 00:00:00 if not with time selection
+		if (null !== $transformed && !$this->withTimeSelection)
+		{
+			$transformed = $transformed->setTime(0, 0);
+		}
+
+		return parent::transformData($transformed, $dataContext, $dataVisitor);
 	}
 }
