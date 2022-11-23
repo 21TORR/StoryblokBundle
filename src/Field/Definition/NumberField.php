8744e94f0b4b2ad8eaa337cafac16cce24079956
Jannik Zschiesche <j.zschiesche@21torr.com>
2022-11-23 21:15:31 +0100

    Add transform data for numbers

--- src/Field/Definition/NumberField.php
+++ src/Field/Definition/NumberField.php
@@ -2,11 +2,23 @@
 
 namespace Torr\Storyblok\Field\Definition;
 
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\Type;
+use Torr\Storyblok\Context\StoryblokContext;
 use Torr\Storyblok\Field\FieldType;
 use Torr\Storyblok\Validator\DataValidator;
+use Torr\Storyblok\Visitor\DataVisitorInterface;
 
 final class NumberField extends AbstractField
 {
+	/**
+	 * @inheritDoc
+	 */
+	public function __construct (string $label, int|float|null $defaultValue = null)
+	{
+		parent::__construct($label, $defaultValue);
+	}
+
 	/**
 	 * @inheritDoc
 	 */
@@ -20,5 +32,38 @@ final class NumberField extends AbstractField
 	 */
 	public function validateData (DataValidator $validator, array $path, mixed $data) : void
 	{
+		$validator->ensureDataIsValid(
+			$path,
+			$this,
+			$data,
+			[
+				// numbers are always passed as strings
+				new Type("string"),
+				new Regex("~^\\d+(\\.\\d+)?$~"),
+			],
+		);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function transformData (
+		mixed $data,
+		StoryblokContext $dataContext,
+		?DataVisitorInterface $dataVisitor = null,
+	) : mixed
+	{
+		if (\is_string($data) && "" !== $data)
+		{
+			$transformed = \str_contains($data, ".")
+				? (float) $data
+				: (int) $data;
+		}
+		else
+		{
+			$transformed = null;
+		}
+
+		return parent::transformData($transformed, $dataContext, $dataVisitor);
 	}
 }
