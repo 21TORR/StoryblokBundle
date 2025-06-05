f9b40e1ce3aec3c37431554b0f44ffe27f902ad6
Manuel Duta <m.duta@21torr.com>
2025-06-05 12:40:32 +0200

    Remove XmlNormalizer

--- src/TranslationManagement/Exception/XmlExportException.php
+++
@@ -1,6 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Torr\Storyblok\TranslationManagement\Exception;
-
-final class XmlExportException extends \RuntimeException implements TranslationManagementExceptionInterface
-{}
