dbae01ec8a65f5478d74767b09d7695ec5557deb
Manuel Duta <m.duta@21torr.com>
2026-02-17 18:14:31 +0100

    Add StoryblokAdapterManager

--- src/Adapter/AbstractStoryblokAdapter.php
+++ src/Adapter/AbstractStoryblokAdapter.php
@@ -1,10 +1,12 @@
 <?php declare(strict_types=1);
 
-namespace Torr\Storyblok\Api\Adapter;
+namespace Torr\Storyblok\Adapter;
 
 use Psr\Log\LoggerInterface;
+use Symfony\Component\RateLimiter\RateLimiterFactoryInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Torr\Storyblok\Api\ContentApi;
+use Torr\Storyblok\Api\ManagementApi;
 use Torr\Storyblok\Component\AbstractComponent;
 use Torr\Storyblok\Config\StoryblokConfig;
 use Torr\Storyblok\Manager\ComponentManager;
@@ -12,37 +14,30 @@ use Torr\Storyblok\Story\StoryFactory;
 
 abstract class AbstractStoryblokAdapter
 {
-	public ContentApi $contentApi;
+	public private(set) ContentApi $contentApi;
+	public private(set) ManagementApi $managementApi;
 
-	/**
-	 * @param string $spaceId         To be defined in extending class as dependency injection
-	 * @param string $managementToken To be defined in extending class as dependency injection
-	 * @param string $contentToken    To be defined in extending class as dependency injection
-	 * @param string $webhookSecret   To be defined in extending class as dependency injection
-	 */
 	public function __construct (
-		string $spaceId,
-		string $managementToken,
-		string $contentToken,
-		string $webhookSecret,
+		StoryblokConfig $config,
 		HttpClientInterface $client,
 		StoryFactory $storyFactory,
 		ComponentManager $componentManager,
+		RateLimiterFactoryInterface $storyblokManagementLimiter,
 		LoggerInterface $logger,
 	)
 	{
-		$config = new StoryblokConfig(
-			spaceId: $spaceId,
-			managementToken: $managementToken,
-			contentToken: $contentToken,
-			webhookSecret: $webhookSecret,
-		);
-
 		$this->contentApi = new ContentApi(
 			$client,
 			$config,
 			$storyFactory,
 			$componentManager,
+			$logger,
+		);
+
+		$this->managementApi = new ManagementApi(
+			$config,
+			$client,
+			$storyblokManagementLimiter,
 			$logger,
 		);
 	}
@@ -52,8 +47,7 @@ abstract class AbstractStoryblokAdapter
 	 */
 	abstract public function getStandaloneComponents () : array;
 
-	public static function getKey () : string
-	{
-		return self::class;
-	}
+	abstract public function getDisplayName () : string;
+
+	abstract public static function getKey () : string;
 }
---
+++ src/Adapter/StoryblokAdapterManager.php
@@ -0,0 +1,62 @@
+<?php declare(strict_types=1);
+
+namespace Torr\Storyblok\Adapter;
+
+use Symfony\Component\DependencyInjection\Attribute\AutowireLocator;
+use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
+use Symfony\Component\DependencyInjection\ServiceLocator;
+use Torr\Storyblok\Exception\Adapter\UnknownAdapterKeyException;
+
+/**
+ * @final
+ */
+readonly class StoryblokAdapterManager
+{
+	public const string DI_TAG = "storyblok.adapter";
+
+	public function __construct (
+		/** @var ServiceLocator<AbstractStoryblokAdapter> */
+		#[AutowireLocator(services: self::DI_TAG, defaultIndexMethod: 'getKey')]
+		private ServiceLocator $storyblokAdapters,
+	) {}
+
+	public function getByKey (string $key) : AbstractStoryblokAdapter
+	{
+		try
+		{
+			$storyblokAdapter = $this->storyblokAdapters->get($key);
+			\assert($storyblokAdapter instanceof AbstractStoryblokAdapter);
+
+			return $storyblokAdapter;
+		}
+		catch (ServiceNotFoundException $exception)
+		{
+			throw new UnknownAdapterKeyException(
+				message: \sprintf(
+					"Unknown storyblok adapter type: %s",
+					$key,
+				),
+				adapterKey: $key,
+				previous: $exception,
+			);
+		}
+	}
+
+	/**
+	 * @return AbstractStoryblokAdapter[]
+	 */
+	public function getAllAdapters () : array
+	{
+		$storyblokAdapters = array_map(
+			fn (string $key) => $this->getByKey($key),
+			array_keys($this->storyblokAdapters->getProvidedServices()),
+		);
+
+		usort(
+			$storyblokAdapters,
+			static fn (AbstractStoryblokAdapter $left, AbstractStoryblokAdapter $right) => strnatcmp($left->getDisplayName(), $right->getDisplayName()),
+		);
+
+		return $storyblokAdapters;
+	}
+}
