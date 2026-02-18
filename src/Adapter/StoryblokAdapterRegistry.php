9295deac8e907b5c9bd44de1324d1f4a3bebe4b6
Manuel Duta <m.duta@21torr.com>
2026-02-18 10:21:52 +0100

    Remove assert

--- src/Adapter/StoryblokAdapterRegistry.php
+++ src/Adapter/StoryblokAdapterRegistry.php
@@ -25,7 +25,6 @@ readonly class StoryblokAdapterRegistry
 		try
 		{
 			$storyblokAdapter = $this->storyblokAdapters->get($key);
-			\assert($storyblokAdapter instanceof AbstractStoryblokAdapter);
 
 			return $storyblokAdapter;
 		}
