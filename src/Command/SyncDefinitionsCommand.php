1181d745abd0aa28a3d898df432d25cddaaa07f3
Manuel Duta <m.duta@21torr.com>
2026-02-20 10:42:59 +0100

    Fix wording

--- src/Command/SyncDefinitionsCommand.php
+++ src/Command/SyncDefinitionsCommand.php
@@ -38,7 +38,7 @@ final class SyncDefinitionsCommand extends Command
 	{
 		$this
 			->setDescription("Syncs the local component definitions to storyblok")
-			->addArgument("adapterKey", InputArgument::OPTIONAL, "Storyblok adapter key")
+			->addArgument("adapterKey", InputArgument::OPTIONAL, "Storyblok adapter key. If not set, all adapters will be synced.")
 			->addOption("force", null, InputOption::VALUE_NONE, "Whether to force sync");
 	}
 
