e2df8e390c611e823614c350a0bebf4ef4cfc547
Jannik Zschiesche <j.zschiesche@21torr.com>
2026-03-04 10:59:56 +0100

    Update tests

--- tests/Manager/ComponentManagerTest.php
+++ tests/Manager/ComponentManagerTest.php
@@ -4,6 +4,10 @@ namespace Tests\Torr\Storyblok\Manager;
 
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\DependencyInjection\ServiceLocator;
+use Tests\Torr\Storyblok\Fixtures\ComponentA;
+use Tests\Torr\Storyblok\Fixtures\ComponentB;
+use Tests\Torr\Storyblok\Fixtures\ComponentC;
+use Tests\Torr\Storyblok\Fixtures\ComponentD;
 use Torr\Storyblok\Component\Filter\ComponentFilter;
 use Torr\Storyblok\Manager\ComponentManager;
 
@@ -12,79 +16,71 @@ use Torr\Storyblok\Manager\ComponentManager;
  */
 final class ComponentManagerTest extends TestCase
 {
+	/**
+	 */
 	public function testGetComponentKeysForTagsOnlyKey () : void
 	{
-		$filter = new ComponentFilter(components: ["test"]);
+		$componentManager = new ComponentManager(new ServiceLocator([
+			"a" => static fn () => new ComponentA(["tag1", "tag2"]),
+			"b" => static fn () => new ComponentB(["tag2", "tag3"]),
+		]));
 
-		$componentManager = $this->getMockBuilder(ComponentManager::class)
-			->setConstructorArgs([new ServiceLocator([])])
-			->onlyMethods(['getComponentKeysForTags'])
-			->getMock();
-
-		$componentManager
-			->expects(self::never())
-			->method('getComponentKeysForTags');
-
-		$components = $componentManager->getComponentKeysForFilter($filter);
+		$components = $componentManager->getComponentKeysForFilter(
+			new ComponentFilter(components: ["a"]),
+		);
 
 		self::assertIsArray($components);
-		self::assertSame(["test"], $components);
+		self::assertSame(["a"], $components);
 	}
 
+	/**
+	 */
 	public function testGetComponentKeysForTagsKeyAndTag () : void
 	{
-		$filter = new ComponentFilter(["tag1"], ["test"]);
+		$filter = new ComponentFilter(["tag3"], ["a"]);
 
-		$componentManager = $this->getMockBuilder(ComponentManager::class)
-			->setConstructorArgs([new ServiceLocator([])])
-			->onlyMethods(['getComponentKeysForTags'])
-			->getMock();
-
-		$componentManager
-			->expects(self::once())
-			->method("getComponentKeysForTags")
-			->with(["tag1"])
-			->willReturn(["transformedTag1", "transformedTag2"]);
+		$componentManager = new ComponentManager(new ServiceLocator([
+			"a" => static fn () => new ComponentA(["tag1", "tag2"]),
+			"b" => static fn () => new ComponentB(["tag2", "tag3"]),
+			"c" => static fn () => new ComponentC(["tag3"]),
+			"d" => static fn () => new ComponentD(["tag4"]),
+		]));
 
 		$components = $componentManager->getComponentKeysForFilter($filter);
 
 		self::assertIsArray($components);
-		self::assertSame(["test", "transformedTag1", "transformedTag2"], $components);
+		self::assertSame(["a", "b", "c"], $components);
 	}
 
+	/**
+	 */
 	public function testGetComponentKeysForTagsEmpty () : void
 	{
 		$filter = new ComponentFilter(tags: ["tag"]);
 
-		$componentManager = $this->getMockBuilder(ComponentManager::class)
-			->setConstructorArgs([new ServiceLocator([])])
-			->onlyMethods(['getComponentKeysForTags'])
-			->getMock();
-
-		$componentManager->method("getComponentKeysForTags")
-			->willReturn([]);
-
+		$componentManager = new ComponentManager(new ServiceLocator([]));
 		$components = $componentManager->getComponentKeysForFilter($filter);
 
 		self::assertIsArray($components);
 		self::assertSame([], $components);
 	}
 
+	/**
+	 */
 	public function testGetComponentKeysForTagsNoDuplicates () : void
 	{
-		$filter = new ComponentFilter(tags: ["tag"], components: ["test"]);
+		$filter = new ComponentFilter(tags: ["tag1"], components: ["a"]);
 
-		$componentManager = $this->getMockBuilder(ComponentManager::class)
-			->setConstructorArgs([new ServiceLocator([])])
-			->onlyMethods(['getComponentKeysForTags'])
-			->getMock();
-
-		$componentManager->method("getComponentKeysForTags")
-			->willReturn(["test"]);
+		$componentManager = new ComponentManager(new ServiceLocator([
+			"a" => static fn () => new ComponentA(["tag1", "tag2"]),
+			"b" => static fn () => new ComponentB(["tag2", "tag3"]),
+			"c" => static fn () => new ComponentC(["tag3"]),
+			"d" => static fn () => new ComponentD(["tag4"]),
+		]));
 
 		$components = $componentManager->getComponentKeysForFilter($filter);
 
 		self::assertIsArray($components);
-		self::assertSame(["test"], $components);
+		self::assertSame(["a"], $components);
 	}
 }
